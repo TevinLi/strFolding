ce113eebca26532f706b65a03b67c0d12ba9424a
TevinLi <6317595@qq.com>
2017-02-14 10:00:49 +0800

    更改替换符算法，中文采用单字替换符，英文数字采用双字替换符

--- src/strFolding.js
+++ src/strFolding.js
@@ -13,8 +13,8 @@
 
     var SF = function () {
         this._data = {
-            list: [],
-            lib2C: [],
+            listEN: [],
+            listCN: [],
             source: '',
             result: ''
         };
@@ -22,21 +22,21 @@
 
     //预转换
     SF.prototype._preTransition = function () {
-        var list = this._data.result.match(/[\u0100-\u014f][0-9A-Za-z]/g);
-        var list2C = this._data.result.match(/[\u014f-\u07e0]/g);
+        var listEN = this._data.result.match(/[\u0100-\u014f][0-9A-Za-z]/g);
+        var listCN = this._data.result.match(/[\u014f-\u07e0]/g);
         //如果不存在替换符相同字符
-        if (!list && !list2C) {
+        if (!listEN && !listCN) {
             return true;
         }
         //如果单字替换符相同字符过多，不再执行压缩
-        if (list2C && list2C > 20) {
+        if (listEN.length > 20 || listCN.length > 20) {
             return false;
         }
         //如果存在替换符相同格式的字符，直接入字典并替换
         var library = {},
             num;
-        list = list.concat(list2C || []);
-        for (var i = 0, item; item = list[i]; i++) {
+        listEN = listEN.concat(listCN || []);
+        for (var i = 0, item; item = listEN[i]; i++) {
             if (typeof library[item] == 'undefined') {
                 library[item] = 1;
                 num = this._data.list.push(item);
@@ -53,43 +53,62 @@
         if (!list) {
             return;
         }
+        //抽取重复字段
+        var duplicates = this._extChineseDuplicate(list);
         //提取样本与单字
         var charsData = this._extChineseChar(list);
         //样本文
         var content = charsData.list2.join(',');
-        //提取两字词根
+        //根据样文本提取两字词根
         var wordsLv2 = [];
         for (var i = 0, char; char = charsData.chars[i]; i++) {
             wordsLv2 = wordsLv2.concat(this._findCharRight(content, char.key));
         }
         i = char = charsData = list = null;
-        //console.log(wordsLv2);
+        //console.log(JSON.stringify(wordsLv2));
         //提取多字词根
-        var wordsData = {};
-        wordsData.wordsLv3 = this._mergeEtyma(wordsLv2, wordsLv2, content);
+        var wordsData = {
+            wordsLv2: wordsLv2
+        };
+        wordsData.wordsLv3 = this._mergeEtyma(wordsData.wordsLv2, wordsLv2, content);
         wordsData.wordsLv4 = this._mergeEtyma(wordsData.wordsLv3, wordsLv2, content);
         wordsData.wordsLv5 = this._mergeEtyma(wordsData.wordsLv4, wordsLv2, content);
         wordsData.wordsLv6 = this._mergeEtyma(wordsData.wordsLv5, wordsLv2, content);
         wordsData.wordsLv7 = this._mergeEtyma(wordsData.wordsLv6, wordsLv2, content);
         wordsData.wordsLv8 = this._mergeEtyma(wordsData.wordsLv7, wordsLv2, content);
+        //console.log(JSON.stringify(wordsData));
+        //console.log(JSON.stringify(duplicates));
+        //合并
+        wordsData = this._extendList(wordsData, duplicates);
+        //console.log(JSON.stringify(wordsData));
         //词根包容计算
-        var wordsTemp = this._charContains(wordsData.wordsLv3, wordsData.wordsLv4);
+        var wordsTemp = this._charContains(wordsLv2, wordsData.wordsLv3);
+        wordsTemp = this._charContains(wordsTemp, wordsData.wordsLv4);
         wordsTemp = this._charContains(wordsTemp, wordsData.wordsLv5);
         wordsTemp = this._charContains(wordsTemp, wordsData.wordsLv6);
         wordsTemp = this._charContains(wordsTemp, wordsData.wordsLv7);
         wordsTemp = this._charContains(wordsTemp, wordsData.wordsLv8);
+        //得分排序
+        wordsTemp.sort(function (a, b) {
+            var aScore = a.key.length * a.num - a.num - a.key.length - 1;
+            var bScore = b.key.length * b.num - b.num - b.key.length - 1;
+            return aScore > bScore ? -1 : 1;
+        });
+        //序号范围约束
+        var wordsRange = parseInt(1713 * .6);
+        if (wordsTemp.length > wordsRange) {
+            wordsTemp.length = wordsRange;
+        }
+        //按长度重新排序
         wordsTemp.sort(function (a, b) {
             return a.key.length > b.key.length ? -1 : 1;
         });
-        //console.log(JSON.stringify(wordsTemp));
+        //console.log(JSON.stringify(wordsTemp).replace(/\},/g, '},\n'), wordsTemp.length);
         //处理
         for (var j = 0, item, num; item = wordsTemp[j]; j++) {
-            num = this._data.list.push(item.key) - 1;
-            this._replaceResult(item.key, num) || this._data.list.pop();
-        }
-        for (i = 0; item = wordsLv2[i]; i++) {
-            num = this._data.lib2C.push(item.key) - 1;
-            this._replaceResult(item.key, num, 'single') || this._data.lib2C.pop();
+            //console.log(item.key);
+            num = this._data.listCN.push(item.key) - 1;
+            this._replaceResult(item.key, num, 'single') || this._data.listCN.pop();
         }
     };
 
@@ -122,7 +141,7 @@
         //大于50的列队按一定量平均抽取汉字段组成新列队
         else {
             //需要抽取的长度
-            var newLong = parseInt(Math.pow(list.length - 50, .8) + 50);
+            var newLong = parseInt(Math.pow(list.length - 50, .82) + 50);
             var dta = list.length / newLong;
             //跳跃式抽取
             for (var i = 0; i < newLong; i++) {
@@ -155,9 +174,7 @@
         list3.sort(function (a, b) {
             return a.num > b.num ? -1 : 1;
         });
-        //去除四分之一低重复字
-        list3 = list3.slice(0, parseInt(list3.length * 3 / 4));
-        //console.log(list3);
+        //console.log(JSON.stringify(list3));
         return {
             list2: list2,
             chars: list3
@@ -216,32 +233,97 @@
         return wordsListRes;
     };
 
+    //基于非汉字分段处理重复的字段
+    SF.prototype._extChineseDuplicate = function (list) {
+        var that = this, i, item, library = {}, lib,
+            wordsDup = {
+                wordsLv2: [],
+                wordsLv3: [],
+                wordsLv4: [],
+                wordsLv5: [],
+                wordsLv6: [],
+                wordsLv7: [],
+                wordsLv8: []
+            };
+        //计算重复次数
+        for (i = 0; item = list[i]; i++) {
+            if (typeof library[item] == 'number') {
+                library[item]++;
+            } else {
+                library[item] = 1;
+            }
+        }
+        //丢弃仅一次出现的词，并分长度存储
+        for (lib in library) {
+            if (library.hasOwnProperty(lib) && library[lib] > 1) {
+                if (library[lib] >= 3 && lib.length <= 8) {
+                    wordsDup['wordsLv' + lib.length].push({
+                        key: lib,
+                        num: library[lib]
+                    });
+                }
+            }
+        }
+        return wordsDup;
+    };
+
+    //两种抽取字典合并
+    SF.prototype._extendList = function(arr1, arr2) {
+        var already = false;
+        var listTemp = [];
+        for (var k = 2; k <= 8; k++) {
+            listTemp.length = 0;
+            for (var i = 0, item2; item2 = arr2['wordsLv' + k][i]; i++) {
+                already = false;
+                for (var j = 0, item1; item1 = arr1['wordsLv' + k][j]; j++) {
+                    if (item1.key == item2.key) {
+                        item1.num = Math.max(item1.num, item2.num);
+                        already = true;
+                        break;
+                    }
+                }
+                if (!already) {
+                    //console.log(JSON.stringify(item2));
+                    listTemp.push(item2);
+                }
+            }
+            //console.log(JSON.stringify(listTemp));
+            arr1['wordsLv' + k] = arr1['wordsLv' + k].concat(listTemp);
+        }
+        return arr1;
+    };
+
     //词根包容计算
     SF.prototype._charContains = function (wordsfrom, wordsLv) {
         var tempList = [],
-            containsHas = false;
+            containsHas = false,
+            item1Reg = null;
         //多字词对少字词包容计算
         for (var i = 0, item1; item1 = wordsfrom[i]; i++) {
             containsHas = false;
+            item1Reg = new RegExp(item1);
             for (var j = 0, item2; item2 = wordsLv[j]; j++) {
                 //当多字词包含少字词
                 if (item2.key.indexOf(item1.key) >= 0) {
+                    containsHas = true;
                     //当少字词计数小于多字词计数，理论上不存在
                     //当两者计数相等，或少字词比多字词计数多1个，丢弃少字词
                     //当少字词计数比多字词多出2个以上，保留少字词
                     if (item1.num > item2.num + 1) {
                         tempList.unshift(item1);
+                        break;
+                        //console.log(item1, item2);
                     }
-                    containsHas = true;
-                    break;
                 }
             }
-            //多字词不包含此少字词，保留三字词
+            //多字词不包含此少字词，保留少字词
             if (!containsHas) {
                 tempList.push(item1);
+                //console.log(item1);
             }
         }
         tempList = tempList.concat(wordsLv);
+        //console.log(JSON.stringify(tempList));
         return tempList;
     };
 
@@ -261,13 +343,8 @@
         //丢弃仅一次出现的词
         for (lib in library) {
             if (library.hasOwnProperty(lib) && library[lib] > 1) {
-                //多字字符串
                 if (lib.length > 2) {
-                    list.push(lib);
-                }
-                //两汉字字符串
-                else if (library[lib] > 2) {
-                    list2C.push({
+                    list.push({
                         key: lib,
                         num: library[lib]
                     });
@@ -275,23 +352,25 @@
             }
         }
         library = lib = i = item = null;
+        //按得分排序
+        list.sort(function (a, b) {
+            var aScore = a.key.length * a.num - a.num - a.key.length - 1;
+            var bScore = b.key.length * b.num - b.num - b.key.length - 1;
+            return aScore > bScore ? -1 : 1;
+        });
+        var wordsRange = 1713 - this._data.listCN.length;
+        if (list.length > wordsRange) {
+            list.length = wordsRange;
+        }
         //多字列表按从长到短排序
         list.sort(function (a, b) {
-            return a.length > b.length ? -1 : 1;
-        });
-        //两字列表按次数排序
-        list2C.sort(function (a, b) {
-            return a.num > b.num ? -1 : 1;
+            return a.key.length > b.key.length ? -1 : 1;
         });
         //处理
         var num;
         for (i = 0; item = list[i]; i++) {
-            num = this._data.list.push(item) - 1;
-            this._replaceResult(item, num) || this._data.list.pop();
-        }
-        for (i = 0; item = list2C[i]; i++) {
-            num = this._data.lib2C.push(item.key) - 1;
-            this._replaceResult(item.key, num, 'single') || this._data.lib2C.pop();
+            num = this._data.listCN.push(item.key) - 1;
+            this._replaceResult(item.key, num) || this._data.listCN.pop();
         }
         //console.log(this._data.lib2C);
     };
@@ -325,8 +404,8 @@
         //处理
         var num;
         for (var j = 0; item = list[j]; j++) {
-            num = this._data.list.push(item);
-            this._replaceResult(item, num - 1) || this._data.list.pop();
+            num = this._data.listEN.push(item);
+            this._replaceResult(item, num - 1) || this._data.listEN.pop();
         }
         num = item = j = null;
     };
@@ -439,15 +518,18 @@
         if (count >= 2 && (str.length >= 3 || count >= 3)) {
             this._data.result = result;
             return true;
+        } else {
+            console.log(str);
         }
         return false;
     };
 
     //压缩数据统计
     SF.prototype._statistics = function (startTime, source, result) {
-        var sLong = source/*.replace(/[^\u0000-\u00ff]/g, 'aa')*/.length;
-        var rLong = result/*.replace(/[^\u0000-\u00ff]/g, 'aa')*/.length;
+        var sLong = source.length;
+        var rLong = result.length;
         console.log(sLong, rLong, (rLong / sLong * 100).toFixed(2) + '%', Date.now() - startTime + 'ms');
+        console.log('en:' + this._data.listEN.length, 'cn:' + this._data.listCN.length);
     };
 
     /**
@@ -457,7 +539,7 @@
      */
     SF.prototype.encode = function (source) {
         if (typeof source != 'string') {
-            throw new Error('StrFolding.js: 参数必须为字符串!');
+            throw new Error('StrFolding.js: 输入必须为字符串！');
         }
         var startTime = Date.now();
         this._data.source = this._data.result = source;
@@ -466,15 +548,15 @@
             this._unDuplicateChinese();
             this._transDigitalLetter();
         } else {
-            throw new Error('StrFolding.js: 太多的非英文字母！本压缩工具仅针对中英文文本使用。')
+            throw new Error('StrFolding.js: 太多的非英文字母！本压缩工具仅针对中英文文本使用。');
         }
         //拼装结果
-        var result = this._data.result + '|||' + this._data.list.join(',') + '|||' + this._data.lib2C.join(',');
+        var result = this._data.result + '|||' + this._data.listEN.join(',') + '|||' + this._data.listCN.join(',');
         //统计效率
         this._statistics(startTime, source, result);
         //立即清理
         this._data.source = this._data.result = '';
-        this._data.list.length = 0;
+        this._data.listEN.length = this._data.listCN.length = 0;
         //返回
         return result;
     };
@@ -486,7 +568,7 @@
      */
     SF.prototype.decode = function (result) {
         if (typeof result != 'string') {
-            throw new Error('StrFolding: The parameter must be a string!');
+            throw new Error('StrFolding: 输入必须为字符串！');
         }
     };
 
